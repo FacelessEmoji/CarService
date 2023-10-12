f90a39983a3b21b78386e5247b48c7a0344f9010
Emoji <studyemoji@mail.ru>
2023-10-12 12:19:58 +0300

    Initial commit

---
+++ src/test/java/rut/miit/carservice/CarServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package rut.miit.carservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
