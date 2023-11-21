0c69b41874436fd7088b9a2f53b993ab98338af7
Emoji <studyemoji@mail.ru>
2023-11-21 14:03:05 +0300

    -added search settings boxes content with placeholders -need to finally fix stupid looking search bar

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -19,6 +19,32 @@ function loadPopularModels() {
   // Необходимо добавить URL для получения данных
 }
 
+
+document.addEventListener('DOMContentLoaded', function() {
+  // Получаем все радиокнопки для выбора сущности поиска
+  var searchEntityRadios = document.querySelectorAll('input[name="searchEntity"]');
+
+  // Функция для обновления видимости критериев поиска
+  function updateSearchCriteria() {
+    // Скрываем все критерии
+    document.querySelectorAll('.criteria').forEach(function(criteria) {
+      criteria.style.display = 'none';
+    });
+
+    // Показываем критерии для выбранной сущности
+    var selectedEntity = document.querySelector('input[name="searchEntity"]:checked').value;
+    document.getElementById('criteria-' + selectedEntity).style.display = 'block';
+  }
+
+  // Назначаем обработчик события на изменение каждой радиокнопки
+  searchEntityRadios.forEach(function(radio) {
+    radio.addEventListener('change', updateSearchCriteria);
+  });
+
+  // Инициализируем критерии поиска при загрузке страницы
+  updateSearchCriteria();
+});
+
 // Инициализация тестовой функции и функций загрузки при открытии страницы
 document.addEventListener('DOMContentLoaded', function() {
   testScriptLoad();
